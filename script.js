579bf808441f8391c6add74c9962a58df023da19
Roland <matleroland@gmail.com>
2024-07-11 18:28:17 +0200

    navbar function working

---
+++ script.js
@@ -0,0 +1,12 @@
+const navElement = document.querySelector(".js-nav");
+const hamburgerElement = document.querySelector(".js-hamburger");
+
+hamburgerElement.addEventListener("click", ()=>{
+    navElement.classList.toggle("nav-open");
+    hamburgerElement.classList.toggle("hamburger-open");
+});
+
+navElement.addEventListener("click", ()=>{
+    navElement.classList.remove("nav-open");
+    hamburgerElement.classList.remove("hamburger-open");
+});
